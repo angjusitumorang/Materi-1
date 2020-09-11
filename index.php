f7bb84dfcda82758a849700c4360ef7a5db6ff88
angjusitumorang <angjusitumorang@gmailcom>
2020-09-11 18:21:02 +0700

    membuat file index.php

---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+
+echo "Animal <php>";
+echo " <hr>";
+
+class gajah{
+	   public $nama,  $jumlah_kaki, $bisa_terbang, $suara;
+	}
+
+	$gajah =  new gajah;
+	$gajah->nama = "posy";
+	$gajah->jumlah_kaki = "4";
+	$gajah->bisa_terbang = "tidak";
+	$gajah->suara = "ehhhh";
+
+	echo "gajah  <br>";
+	echo "nama : $gajah->nama <br>";
+	echo "jumlah kaki : $gajah->jumlah_kaki <br>";
+	echo "bisa terbang : $gajah->bisa_terbang <br>";
+	echo "suara : $gajah->suara <br>";
+	echo "<hr>";
+
+class kelinci{
+		public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+	}
+
+	$kelinci = new kelinci;
+	$kelinci->nama = "pusy";
+	$kelinci->jumlah_kaki = "4";
+	$kelinci->bisa_terbang = "tidak";
+	$kelinci->suara = "cicicit";
+
+
+	echo "kelinci  <br>";
+	echo "nama : $kelinci->nama <br>";
+	echo "jumlah kaki : $kelinci->jumlah_kaki <br>";
+	echo "bisa terbang : $kelinci->bisa_terbang <br>";
+	echo "suara : $kelinci->suara <br>";
+	echo "<hr>";
+
+
+
+class buaya{
+		public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+	}
+
+	$buaya = new buaya;
+	$buaya->nama = "ciko";
+	$buaya->jumlah_kaki = "4";
+	$buaya->bisa_terbang = "tidak";
+	$buaya->suara = "ngah";
+
+	echo "buaya  <br>";
+	echo "nama : $buaya->nama <br>";
+	echo "jumlah kaki : $buaya->jumlah_kaki <br>";
+	echo "bisa terbang : $buaya->bisa_terbang <br>";
+	echo "suara : $buaya->suara <br>";
+	echo "<hr>";
+
+class angsa{
+		public $nama, $jumlah_kaki, $bisa_terbang, $suara;
+	}
+
+	$angsa = new angsa;
+	$angsa->nama = "behi";
+	$angsa->jumlah_kaki = "2";
+	$angsa->bisa_terbang = "ya";
+	$angsa->suara = "ngwak ngwak";
+
+	echo "angsa <br>";
+	echo "nama : $angsa->nama <br>";
+	echo "jumlah kaki : $angsa->jumlah_kaki <br>";
+	echo "bisa terbang : $angsa->bisa_terbang <br>";
+	echo "suara : $angsa->suara <br>";
+	echo "<hr>";
+
